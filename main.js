3e37df259a2a16a77584fa01869643795c9b9ce3
ReynaldArro <sleight.nald@gmail.com>
2023-03-16 13:22:13 +0800

    first commit

---
+++ main.js
@@ -0,0 +1,195 @@
+let patientArray = [];
+
+function savePatientForm() {
+    let fName = document.getElementById(`fName`).value;
+    let mName = document.getElementById(`mName`).value;
+    let lName = document.getElementById(`lName`).value;
+    let address = document.getElementById(`address`).value;
+    let mobileNum = document.getElementById(`mobileNum`).value;
+    let birthDate = document.getElementById(`birthDate`).value;
+    let gender = document.querySelector('input[name="gender"]:checked').value;
+
+    let medHistoryCheckboxes = document.querySelectorAll('input[name="medHistory"]');
+    let medHistory = [];
+
+    for (let i = 0; i < medHistoryCheckboxes.length; i++) {
+        let checkbox = medHistoryCheckboxes[i];
+        if (checkbox.checked) {
+            medHistory.push(checkbox.value);
+        }
+    }
+
+    let currSymptomsSelect = document.getElementById('currSymptoms');
+    let currSymptoms = [];
+
+    for (let i = 0; i < currSymptomsSelect.selectedOptions.length; i++) {
+        currSymptoms.push(currSymptomsSelect.selectedOptions[i].value);
+    }
+    
+    let takeMedsRadio = document.querySelector('input[name="yesorno"]:checked');
+    let takeMeds = "";
+
+    if (takeMedsRadio && takeMedsRadio.value === "yes") {
+        takeMeds = document.getElementById("meds").value;
+    }
+
+    let patient = {
+        patientId: Date.now(),
+        firstName: fName,
+        middleName: mName,
+        lastName: lName,
+        address: address,
+        mobileNum: mobileNum,
+        birthDate: birthDate,
+        gender: gender,
+        medHistory: medHistory,
+        currSymptoms: currSymptoms,
+        takeMeds: takeMeds,
+    }
+    
+    
+    localStorage.setItem(Date.now(), JSON.stringify(patient));
+    
+}
+
+
+  
+// Form Validation
+let form = document.getElementById(`patientForm`);
+
+form.addEventListener('submit', function(event) {
+    
+    event.preventDefault();
+    
+    if (fName.value.trim() === '') {
+      alert('First name is required.');
+      fName.focus();
+      return false;
+    }
+  
+    
+    if (lName.value.trim() === '') {
+      alert('Last name is required.');
+      lName.focus();
+      return false;
+    }
+
+
+    if (address.value.trim() === '') {
+        alert('Address is required.');
+        address.focus();
+        return false;
+    }
+  
+    
+    if (birthDate.value.trim() === '') {
+      alert('Birth date is required.');
+      birthDate.focus();
+      return false;
+    }
+  
+    
+    let isGenderSelected = false;
+    for (let i = 0; i < gender.length; i++) {
+      if (gender[i].checked) {
+        isGenderSelected = true;
+        break;
+      }
+    }
+    if (!isGenderSelected) {
+      alert('Gender is required.');
+      gender[0].focus();
+      return false;
+    }
+  
+    
+    if (mobileNum.value.trim() === '') {
+      alert('Mobile number is required.');
+      mobileNum.focus();
+      return false;
+    }
+  
+    
+    alert('Form submitted successfully.');
+    form.submit();
+  });
+
+
+  function retrievePatientInfo(patientId) {
+    let patient = JSON.parse(localStorage.getItem(patientId));
+    if (!patient) {
+      return null;
+    }
+    return patient;
+  }
+
+
+function openModal() {
+    
+    let patientId = prompt("Enter patient ID:");
+    let patient = retrievePatientInfo(patientId);
+    let modal = document.getElementById("myModal");
+    let patientInfoElem = document.getElementById("patientInfo");
+
+    if (patient) {
+      let patientInfo = `
+        Patient ID: ${patient.patientId}
+        <br>First Name: ${patient.firstName}
+        <br>Middle Name: ${patient.middleName}
+        <br>Last Name: ${patient.lastName}
+        <br>Address: ${patient.address}
+        <br>Mobile Number: ${patient.mobileNum}
+        <br>Birth Date: ${patient.birthDate}
+        <br>Gender: ${patient.gender}
+        <br>Medical History: ${patient.medHistory.join(", ")}
+        <br>Current Symptoms: ${patient.currSymptoms.join(", ")}
+      `;
+
+      if (patient.takeMeds !== "") {
+        patientInfo += `<br>Taking Medications: ${patient.takeMeds}`;
+      }
+
+      patientInfoElem.innerHTML = patientInfo;
+    } else {
+      patientInfoElem.innerHTML = "Patient not found.";
+    }
+    
+    modal.style.display = "block";
+    
+    
+    let closeBtn = modal.querySelector(".close");
+    closeBtn.addEventListener("click", function() {
+      modal.style.display = "none";
+    });
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+window.onload = function() {
+    let patientForm = document.getElementById(`patientForm`);
+    patientForm.onsubmit = savePatientForm; 
+}
